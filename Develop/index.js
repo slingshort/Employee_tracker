38448a9438bda16b09b9cd9acc92c4b96414c16a
slingshort <salina.myat@gmail.com>
2022-10-24 10:26:18 +1100

    first commit

---
+++ Develop/index.js
@@ -0,0 +1,70 @@
+const inquirer = require('inquirer')
+// create a const to require a list of employees as an array to be accessed by npm inquirer
+
+const initPrompt = [
+    {
+        type: 'list',
+        name: 'menu',
+        message: 'Please select from the following options',
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add department', 'Add role', 'Add Employee', 'Quit']
+    }
+]
+
+const addRole = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the job role called?'
+    },
+    {
+        type: 'input',
+        name: 'salary',
+        message: 'What is the salary for this role?'
+    },
+
+    {
+        type: 'list',
+        name: 'department',
+        message: 'Which department is this role for?'
+        // choices with department array from database
+    }
+]
+
+const addEmployee = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the first name of the employee?'
+    },
+    {
+        type: 'input',
+        name: 'lastname',
+        message: 'What is the last name of the employee?'
+    }, 
+    {
+        type: 'input',
+        name: 'role',
+        message: 'What is the role of this employee?'
+    },
+    {
+        type: 'input',
+        name: 'manager',
+        message: 'Who is the manager for this employee?'
+        // managers
+    }
+]
+
+const updateEmployee = [
+    {
+        type: 'list',
+        name: 'update',
+        message: 'Please choose which employee you would like to update information for',
+        // choices: variable globally stored
+    },
+    {
+        type: 'list',
+        name: 'selectfrom',
+        message: 'Which role do you want to assign the chosen employee to?',
+        // choices: variable globally stored.ß
+    }
+]
